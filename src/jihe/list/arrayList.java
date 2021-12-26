93a0f4e32d245ead02803a04660687ce5405490e
DESKTOP-IC0782V\刘江 <2569736267@qq.com>
2021-12-26 12:29:56 +0800

    默认的

--- src/jihe/list/arrayList.java
+++ src/jihe/list/arrayList.java
@@ -1,5 +1,8 @@
 package jihe.list;
 
+import shuzu.Person;
+import shuzu.test;
+
 import java.util.ArrayList;
 
 /**
@@ -12,6 +15,56 @@ import java.util.ArrayList;
  */
 public class arrayList {
     public static void main(String[] args) {
-        ArrayList<Object> arrayList = new ArrayList<>();
+        ArrayList<Person> arrayList = new ArrayList<Person>();
+
+        arrayList.add(new Person("关羽", 18));
+        arrayList.add(new Person("张飞", 18));
+        arrayList.add(new Person("赵云", 18));
+        arrayList.add(new Person("黄忠", 18));
+        arrayList.add(new Person("马超", 18));
+       //     tolist(arrayList);
+        //test.tolist2(arrayList);
+        System.out.println(arrayList);
+        /*for (Person item : arrayList) {
+            arrayList.remove(item);
+            System.out.println(item);
+        }*/
+        arrayList.forEach(obj ->
+                        arrayList.remove(obj)
+        );
+
+
+
+        ArrayList  arrayListint = new ArrayList();
+
+        arrayListint.add(new Integer(1));
+        arrayListint.add(new Integer(2));
+        arrayListint.add(new Integer(3));
+        arrayListint.add(new Integer(4));
+        arrayListint.add(new Integer(5));
+        tolist(arrayList);
+        System.out.println(arrayListint);
+
+
+
+
+
+
     }
+
+       public static void tolist (ArrayList<Person> list){
+           list.remove(0);
+           list.get(1).setAge(50);
+           list.add(new Person("刘备", 18));
+
+
+       }
+
+    public static void tolist1 (ArrayList list){
+        list.remove(0);
+        list.add(new Person("刘备", 18));
+        list.add(new Integer(6));
+
+    }
+
 }
---
+++ src/jihe/map/RBTree.java
@@ -0,0 +1,15 @@
+package jihe.map;
+
+/**
+ * @Title:
+ * @author: Liu jiang
+ * @Date: 2021-12-26 12:24
+ * @FilePath: TextDemo  ==> RBTree
+ * @Good Mood: Today is another beautiful day！
+ * @Study Code: Did you study today？
+ */
+public class RBTree {
+
+
+
+}
